c3bb70b10507a0361212fd99fafdaf7bf7c5c8b2
sweoo <swezinoo410@gmail.com>
2024-02-16 21:24:48 +0800

    add internal documentation for main.cpp file

--- Section-1/main.cpp
+++ Section-1/main.cpp
@@ -1,4 +1,12 @@
-// code for section-1
+/** @file main.cpp
+ * @author Swe Zin Oo
+ * @date 16 Feb 2024
+ * @brief This 'main.cpp' file is designed to sort arrays in three different orders: random, ascending, and reverse. It then calculates the time 
+ * taken to perform each sorting operation. To analyze the impact of array size and initial data order, the program evaluates arrays of three different sizes: 
+ * Hundred Thousand, One Million, and Ten Million elements. For each size, ten arrays are sorted in each of the three orders, and the execution times 
+ * are stored in a two-dimensional array to present the final results. This file utilize the functions from "arrayUtil.h" file to achieve the desired result.
+*/
+
 #include <iostream>
 #include <cstdlib> 
 #include <chrono>
@@ -24,13 +32,13 @@ int main(){
     ///// row represents three sorting orders which is random, ascending and inverse
     //// column represents ten arrays which will be executed for each sorting order
     int executedTimeForSorting_hundredThousand[NUM_OF_SORTING_ORDER][NUM_OF_ARRAYS];
-    //// ****** Store excuted time for ten arrays for each order into two dimensional array ****** ////
-    //// in which each array has a size of hundred thousand and fill with integer values
+    //// ****** Store excuted time for sorting the array in various orders into two dimensional array ****** ////
+    //// in which the array has a size of hundred thousand and fill with integer values
     storeExecutedTimeIntoTwoDimensionArray(executedTimeForSorting_hundredThousand,hundredThousand_array,HUNDRED_THOUSAND);
     //// ****** Print Two Dimension array as Figure ****** ////
     printFigureForExecutedTime(executedTimeForSorting_hundredThousand);
 
-    cout << right << setw(50) << "--------------------------------------------------" << " HUNDRED THOUSAND ARRAY SIZE " << "---------------------------------------------------" << right << setw(50);
+    cout << right << setw(50) << "--------------------------------------------------" << " HUNDRED THOUSAND ARRAY SIZE " << "---------------------------------------------" << right << setw(50);
     cout << endl;
     cout << endl;
 
@@ -45,13 +53,13 @@ int main(){
     ///// row represents three sorting orders which is random, ascending and inverse
     //// column represents ten arrays which will be executed for each sorting order
     int executedTimeForSorting_onemillion[NUM_OF_SORTING_ORDER][NUM_OF_ARRAYS];
-    //// ****** Store excuted time for ten arrays for each order into two dimensional array ****** ////
-    //// in which each array has a size of one million and fill with integer values
+    //// ****** Store excuted time for sorting the array in various orders into two dimensional array ****** ////
+    //// in which the array has a size of one million and fill with integer values
     storeExecutedTimeIntoTwoDimensionArray(executedTimeForSorting_onemillion,onemillion_array,ONE_MILLION);
      //// ****** Print Two Dimension array as Figure ****** ////
     printFigureForExecutedTime(executedTimeForSorting_onemillion);
 
-    cout << right << setw(50) << "--------------------------------------------------" << " ONE MILLION ARRAY SIZE " << "---------------------------------------------------" << right << setw(50);
+    cout << right << setw(50) << "--------------------------------------------------" << " ONE MILLION ARRAY SIZE " << "--------------------------------------------------" << right << setw(50);
     cout << endl;
     cout << endl;
 
@@ -66,13 +74,13 @@ int main(){
     ///// row represents three sorting orders which is random, ascending and inverse
     //// column represents ten arrays which will be executed for each sorting order
     int executedTimeForSorting_tenmillion[NUM_OF_SORTING_ORDER][NUM_OF_ARRAYS];
-    //// ****** Store excuted time for ten arrays for each order into two dimensional array ****** ////
-    //// in which each array has a size of ten million and fill with integer values
+    //// ****** Store excuted time for sorting the array in various orders into two dimensional array ****** ////
+    //// in which the array has a size of ten million and fill with integer values
     storeExecutedTimeIntoTwoDimensionArray(executedTimeForSorting_tenmillion,tenmillion_array,TEN_MILLION);
      //// ****** Print Two Dimension array as Figure ****** ////
     printFigureForExecutedTime(executedTimeForSorting_tenmillion);
 
-    cout << right << setw(50) << "--------------------------------------------------" << " TEN MILLION ARRAY SIZE " << "---------------------------------------------------" << right << setw(50);
+    cout << right << setw(50) << "--------------------------------------------------" << " TEN MILLION ARRAY SIZE " << "--------------------------------------------------" << right << setw(50);
     cout << endl;
     cout << endl;
 
